974e803403fb9daca6535b441a4db98ed67f7ab4
codernoyan <coder.noyan@gmail.com>
2023-02-28 10:07:30 +0600

    added preventdefault to anchor tag for page refresh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,16 @@ export default function Navbar({setRender}) {
     return prev + cur.quantity;
   }, 0)
 
+  const handleToggleProducts = (e)=>{
+    e.preventDefault()
+    setRender('products')
+  }
+
+  const handleToggleCart = (e)=>{
+    e.preventDefault()
+    setRender('cart')
+  }
+
   return (
     <nav className="bg-[#171C2A] py-4">
       <div className="navBar">
@@ -14,11 +24,11 @@ export default function Navbar({setRender}) {
           <img src="./images/logo.png" alt="LWS" className="max-w-[140px]" />
         </a>
         <div className="flex gap-4">
-          <a onClick={()=> setRender('products')} 
-          // href="#home" 
+          <a onClick={handleToggleProducts} 
+          href="#home" 
           className="navHome" id="lws-home"> Home </a>
-          <a onClick={()=> setRender('cart')} 
-          // href="cart.html" 
+          <a onClick={handleToggleCart} 
+          href="cart.html" 
           className="navCart" id="lws-cart">
             <i className="text-xl fa-sharp fa-solid fa-bag-shopping" />
             <span id="lws-totalCart">{totalQuantity}</span>
